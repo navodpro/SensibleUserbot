73eeba5db0eedc7a500cda5bc78b2cf10910367a
navodpro <66838043+navodpro@users.noreply.github.com>
2020-12-16 23:30:41 -0800

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -19,7 +19,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "© Sensible Userbot Help",
+                "© Navod Thiekshana",
                 text="{}\nCurrently Loaded Plugins: 312".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
@@ -38,7 +38,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/sensible_userbot
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use others\n  Join @Sensible_userbot for learning how to get userbot!"
+            reply_pop_up_alert = "ඔයාට බොටෙක් ඕනනම් \n ඔයා එකක් හදාගන්න!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -57,7 +57,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own Sensible Userbot , don't use others's\n Join  for learning how to get userbot!"
+            reply_pop_up_alert = "ඔයාට බොටෙක් ඕනනම් \n ඔයා එකක් හදාගන්න!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
